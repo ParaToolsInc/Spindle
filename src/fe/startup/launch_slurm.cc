a6842b66d023fc3b3a830fa90fffd89fdf228c39
Nicholas Chaimov <nchaimov@paratools.com>
2026-05-13 09:41:55 -0700

    Fall back to old behavior for Slurm before 23.11

--- src/fe/startup/launch_slurm.cc
+++ src/fe/startup/launch_slurm.cc
@@ -14,6 +14,7 @@ program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place, Suite 330, Boston, MA 02111-1307 USA
 */
 
+#include "config.h"
 #include "launcher.h"
 #include "spindle_debug.h"
 
@@ -153,14 +154,21 @@ bool SlurmLauncher::spawnDaemon()
       return false;
    }
    else if (daemon_pid == 0) {
-      int total_args = 10 + daemon_argc;
+      int total_args = 12 + daemon_argc;
       char **new_daemon_args = (char **) malloc(total_args * sizeof(char *));
       int i = 0;
       char count_buffer[64];
       snprintf(count_buffer, 64, "%d", nnodes);
       new_daemon_args[i++] = const_cast<char *>("srun");
+#if HAVE_SRUN_EXTERNAL_LAUNCHER
       new_daemon_args[i++] = const_cast<char *>("--external-launcher");
       new_daemon_args[i++] = const_cast<char *>("--wait=0");
+#else
+      new_daemon_args[i++] = const_cast<char *>("--ntasks-per-node=1");
+      new_daemon_args[i++] = const_cast<char *>("--wait=0");
+      new_daemon_args[i++] = const_cast<char *>("--gres=none");
+      new_daemon_args[i++] = const_cast<char *>("--mem=0");
+#endif
       new_daemon_args[i++] = const_cast<char *>("-n");
       new_daemon_args[i++] = count_buffer;
       new_daemon_args[i++] = const_cast<char *>("-N");
